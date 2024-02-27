8ce387d8d24ef275a68d03ac9ad0e06c59d89ef9
Andy Dang <andy@whylabs.ai>
2024-02-27 15:54:50 -0800

    Update package structure

---
+++ openllmtelemetry/instrumentation/openai/__init__.py
@@ -0,0 +1,28 @@
+from typing import Collection
+
+from opentelemetry.instrumentation.instrumentor import BaseInstrumentor
+
+from openllmtelemetry.instrumentation.openai.utils import is_openai_v1
+from openllmtelemetry.instrumentation.openai.v0 import OpenAIV0Instrumentor
+from openllmtelemetry.instrumentation.openai.v1 import OpenAIV1Instrumentor
+
+_instruments = ("openai >= 0.27.0",)
+
+
+class OpenAIInstrumentor(BaseInstrumentor):
+    """An instrumentor for OpenAI's client library."""
+
+    def instrumentation_dependencies(self) -> Collection[str]:
+        return _instruments
+
+    def _instrument(self, **kwargs):
+        if is_openai_v1():
+            OpenAIV1Instrumentor().instrument(**kwargs)
+        else:
+            OpenAIV0Instrumentor().instrument(**kwargs)
+
+    def _uninstrument(self, **kwargs):
+        if is_openai_v1():
+            OpenAIV1Instrumentor().uninstrument(**kwargs)
+        else:
+            OpenAIV0Instrumentor().uninstrument(**kwargs)
