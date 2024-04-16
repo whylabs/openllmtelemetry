30a0890fa2107da6081ef85d256483c28ea70375
Jamie Broomall <jamie@whylabs.ai>
2024-04-16 09:23:35 -0700

    Fix client parameters

--- openllmtelemetry/secure.py
+++ openllmtelemetry/secure.py
@@ -30,12 +30,12 @@ class WhyLabsSecureApi(object):
         self._api_key = guard_api_key
         self._dataset_id = dataset_id
         self._client = AuthenticatedClient(
-            _base_url=guard_endpoint,  #
+            base_url=guard_endpoint,  # type: ignore
             token=guard_api_key,  #
             prefix="",  #
-            auth_header_name=auth_header_name,  #
-            _timeout=Timeout(timeout, read=timeout),
-        )
+            auth_header_name=auth_header_name,  # type: ignore
+            timeout=Timeout(timeout, read=timeout), # type: ignore
+        )  # type: ignore
 
     def eval_prompt(self, prompt: str) -> Optional[EvaluationResult]:
         profiling_request = LLMValidateRequest(prompt=prompt, dataset_id=self._dataset_id)
